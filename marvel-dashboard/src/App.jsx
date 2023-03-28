328bf45bc5cc72e16531ce8c0d15f1877cf8b824
Jeremiah Anim <jjanim524@gmail.com>
2023-03-27 22:22:58 -0400

    completed search function

--- marvel-dashboard/src/App.jsx
+++ marvel-dashboard/src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [list, setList] = useState(null);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
   const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = list.data.results.filter((item) =>
+        item.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(list.data.results);
+    }
+  };
+
+  useEffect(() => {
+    const fetchMarvelCharacters = async () => {
+      const response = await fetch(
+        "https://gateway.marvel.com/v1/public/characters?apikey=" + API_KEY
+      );
+      const json = await response.json();
+      setList(json);
+    };
+    fetchMarvelCharacters().catch(console.error);
+  }, []);
+
+  console.log(list.data.results);
   return (
     <div className="App">
-      <h1>Boilerplate</h1>
+      <h1>Marvel Dash!</h1>
+      <p>List of Marvel Characters</p>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(inputString) => searchItems(inputString.target.value)}
+      />
+      {searchInput.length > 0
+        ? filteredResults.map((item, index) => <li key={index}>{item.name}</li>)
+        : list &&
+          list.data.results.map((item, index) => (
+            <li key={index}>{item.name}</li>
+          ))}
     </div>
   );
 }
